a3c1d26b8de12a06e611040adeefded6d31c54c6
Haixiang <haixiang6123@gmail.com>
2021-03-20 13:47:58 +0800

    feat: add comments

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import Tabs from './components/Tabs'
 function App() {
   return (
     <Router>
-      <Tabs/>
-
       <Suspense fallback={<div>Loading...</div>}>
+        {/* Header tabs */}
+        <Tabs/>
+
+        {/* Route content */}
         <Switch>
-          <Route path="/page1" component={lazy(() => import(/* webpackPrefetch: true */ './pages/Page1'))} />
-          <Route path="/page2" component={lazy(() => import(/* webpackPrefetch: true */ './pages/Page2'))} />
+          <Route path="/page1" component={lazy(() => import(/* webpackPrefetch: true */ './pages/Page1'))}/>
+          <Route path="/page2" component={lazy(() => import(/* webpackPrefetch: true */ './pages/Page2'))}/>
         </Switch>
       </Suspense>
     </Router>
